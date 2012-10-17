34593571be43745ce76d758a2d4fed58566ddf5b
Sidhavratha <sidhavratha@gmail.com>
2012-10-17 08:21:43 +0530

    Graph Coloring - Vertex.equals not working

--- JavaProject/src/saif/ms/greedycoloring/GraphColoring.java
+++ JavaProject/src/saif/ms/greedycoloring/GraphColoring.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -19,12 +20,24 @@ public class GraphColoring {
 	/**
 	 * @param args
 	 */
+	
+	private static List<Color> colors = new ArrayList<Color>();
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new FileReader(new File("input_graph_coloring.txt")));
 		int nodeCount = Integer.parseInt(br.readLine());
-		
-		Graph graph = new Graph();
+		Graph graph = Graph.createGraph();
+		StringTokenizer st = null;
+		String currConnection = null;
+		while((currConnection=br.readLine())!=null){
+			st = new StringTokenizer(currConnection," ");
+			Vertex v1 = new Vertex(st.nextToken());
+			Vertex v2 = new Vertex(st.nextToken());
+			Graph.addEdge(graph, v1, v2);
+		}
+
+		int colorused = Graph.assignColor(graph);
+		Graph.printGraph(graph,colorused,"output_graph_coloring.txt");
 		
 	}
 	
@@ -75,15 +88,84 @@ public class GraphColoring {
 			newGraph.bag=bag;
 			return newGraph;
 		}
+		
+		public static Color chooseColor(Graph graph,Vertex vertex){
+			List<Color> possibleColors = new ArrayList<Color>();
+			for(Color avlColor : colors){
+				possibleColors.add(avlColor);
+			}
+			for(Vertex currNghVertex : graph.bag.get(vertex)){
+				if(null!=currNghVertex.color){
+					possibleColors.remove(currNghVertex.color);
+				}
+			}
+			
+			if(possibleColors.isEmpty()){
+				return requestNewColor();
+			}
+			
+			return possibleColors.get(0);
+		}
+		
+		public static int assignColor(Graph graph){
+			int totalColors = 0;
+			for(Vertex vertex : graph.bag.keySet()){
+				int oldColorsSize = colors.size();
+				vertex.color=chooseColor(graph, vertex);
+				int newColorsSize = colors.size();
+				totalColors+=(newColorsSize-oldColorsSize);
+			}
+			
+			return totalColors;
+		}
+		
+		public static void printGraph(Graph graph, int colorsUsed, String outputFile){
+			System.out.println(colorsUsed);
+			for(Vertex vertex : graph.bag.keySet()){
+				System.out.print("("+vertex.id+","+vertex.color.name+"):");
+				boolean first=true;
+				for(Vertex childVertex : graph.getNeighbours(graph, vertex)){
+					if(!first)System.out.print(",");
+					System.out.print(childVertex.id);
+				}
+				System.out.println();
+			}
+		}
+		
+		private static Color requestNewColor(){
+			Color newColor = new Color(colors.size()+"");
+			colors.add(newColor);
+			return newColor;
+		}
+		
+		
 	}
 	
 	static class Vertex{
-		private String id;
+		public String id;
+		public Color color;
 		public Vertex(String id){
 			this.id=id;
 		}
 		public boolean equals(Object vertex){
 			if(vertex instanceof Vertex && ((Vertex) vertex).id.equalsIgnoreCase(this.id)){
+				return true;
+			}
+			return false;
+		}
+	}
+	
+	static class Color{
+		public String id;
+		public String name;
+		
+		public Color(String id){
+			this.id=id;
+			this.name="Color-"+id;
+		}
+		
+		public boolean equals(Object color){
+			if(color instanceof Color && ((Color) color).id.equalsIgnoreCase(this.id)){
 				return true;
 			}
 			return false;
